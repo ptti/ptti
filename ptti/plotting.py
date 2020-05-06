c97b64ce2be5347d2544e85b63a7ba7bd4a6a65e
William Waites <ww@groovy.net>
2020-05-06 21:42:41 +0100

    add time (month) on axis

--- ptti/plotting.py
+++ ptti/plotting.py
@@ -1,7 +1,10 @@
 __all__ = ["plot", "plot_defaults"]
 
 import matplotlib.pyplot as plt
-import matplotlib.colors as mc
+import matplotlib.colors as mcolors
+import matplotlib.dates as mdates
+
+from datetime import datetime
 import numpy as np
 from glob import glob
 import yaml
@@ -23,9 +26,9 @@ yaml_plot_defaults = """
 
 plot_defaults = yaml.load(yaml_plot_defaults, yaml.FullLoader)
 
-def plot(model, output, plots, interventions, title, envelope=True, **unused):
+def plot(model, output, plots, interventions, title, envelope=True, start=None, **unused):
 
-    colours = [mc.to_rgb(c) for c in mc.TABLEAU_COLORS.values()]
+    colours = [mcolors.to_rgb(c) for c in mcolors.TABLEAU_COLORS.values()]
 
     arrays = [np.loadtxt(tsvfile, delimiter="\t")
               for tsvfile in glob("{}*.tsv".format(output))]
@@ -33,6 +36,12 @@ def plot(model, output, plots, interventions, title, envelope=True, **unused):
     trajectories = [a[:,1:] for a in arrays]
 
     time = times[0]
+
+    if start is not None:
+        time_offset = datetime.strptime(start, "%Y/%m/%d").toordinal()
+        time += time_offset
+    else:
+        time_offset = 0
 
     for plot in plots:
         fig, ax = plt.subplots(dpi=300)
@@ -58,10 +67,19 @@ def plot(model, output, plots, interventions, title, envelope=True, **unused):
                 ax.plot(time, meanseries, lw=1.0, color=colour, label=ts["title"])
 
         for intv in interventions:
-            ax.axvline(intv["time"], c=(0, 0, 0), lw=0.5, ls='--')
+            ax.axvline(intv["time"] + time_offset, c=(0, 0, 0), lw=0.5, ls='--')
 
         ax.set_title("{}: {}".format(title, plot["title"]))
-        ax.set_xlabel("Time")
+
+        if start is not None:
+            months = mdates.MonthLocator()
+            ym_fmt = mdates.DateFormatter('%Y/%m')
+            ax.xaxis.set_major_locator(months)
+            ax.xaxis.set_major_formatter(ym_fmt)
+            fig.autofmt_xdate()
+        else:
+            ax.set_xlabel("Days since start of outbreak")
+
         ax.legend()
 
         plt.savefig("{}-{}.png".format(output, plot["name"]))
