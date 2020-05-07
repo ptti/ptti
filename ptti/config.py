ca5dabdd82fd0b058716aacf51fbbc98168fb01e
William Waites <ww@groovy.net>
2020-05-07 09:27:17 +0100

    enforce ordering on config dictionaries so that arithmetic works

--- ptti/config.py
+++ ptti/config.py
@@ -1,8 +1,20 @@
 __all__ = ["config_load"]
 
 from ptti.plotting import plot_defaults
+from collections import OrderedDict
 import numpy as np
 import yaml
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
 
 def numpy_funcs():
     funcs = ['beta', 'binomial', 'chisquare', 'choice', 'dirichlet', 'exponential', 'gamma',
@@ -31,7 +43,7 @@ def config_load(filename=None, sample=0):
     """
     if filename is not None:
         with open(filename) as fp:
-            cfg = yaml.load(fp.read(), yaml.FullLoader)
+            cfg = ordered_load(fp.read(), yaml.FullLoader)
     else:
         cfg = {}
 
@@ -46,9 +58,7 @@ def config_load(filename=None, sample=0):
 
         ## compute global parameters
         if k == "parameters":
-            params = _eval_params(v, gvars)
-            gvars.update(params)
-            v.update(params)
+            v.update(_eval_params(v, gvars))
 
         if k == "interventions":
             for intv in v:
@@ -78,10 +88,15 @@ def config_load(filename=None, sample=0):
     return cfg
 
 def _eval_params(d, gvars):
+    """
+    Warning, mutates the gvars dictionary by adding parameters into it
+    """
     params = {}
     for k, v in d.items():
         if isinstance(v, str):
             params[k] = eval(v, gvars)
         else:
             params[k] = v
+        gvars[k] = params[k]
+        #print("setting {} to {} = {}".format(k, v, params[k]))
     return params
