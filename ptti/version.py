61a117267e811000e085af55b5929d6e90d5851f
William Waites <ww@groovy.net>
2020-05-07 13:14:40 +0100

    add software metadata into config meta

---
+++ ptti/version.py
@@ -0,0 +1,15 @@
+__all__ = ["distribution", "software", "python", "platform", "revision"]
+
+import pkg_resources
+import sys
+import git
+
+platform = pkg_resources.get_platform()
+python = sys.version
+distribution = pkg_resources.get_distribution(__name__.split(".")[0])
+
+software = "{} {}".format(distribution.project_name, distribution.version)
+
+repo = git.Repo(search_parent_directories=True)
+revision = repo.head.object.hexsha
+
