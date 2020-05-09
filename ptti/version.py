4c92e0234864ffcf28464f9420acdc014df2d015
William Waites <wwaites@inf.ed.ac.uk>
2020-05-09 15:13:37 +0100

    do not error if run outwith the git tree

--- ptti/version.py
+++ ptti/version.py
@@ -10,6 +10,9 @@ distribution = pkg_resources.get_distribution(__name__.split(".")[0])
 
 software = "{} {}".format(distribution.project_name, distribution.version)
 
-repo = git.Repo(search_parent_directories=True)
-revision = repo.head.object.hexsha
+try:
+    repo = git.Repo(search_parent_directories=True)
+    revision = repo.head.object.hexsha
+except git.exc.InvalidGitRepositoryError:
+    revision = "UNKNOWN"
 
