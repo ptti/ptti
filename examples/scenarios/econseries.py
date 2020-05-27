9ccf1325a4f914b55157efbadc11cc4c8b295ee4
William Waites <ww@groovy.net>
2020-05-27 13:52:14 +0100

    table output

--- examples/scenarios/econseries.py
+++ examples/scenarios/econseries.py
@@ -29,3 +29,23 @@ for (times, dat, fn) in [(trtimes, trdat, slug+"-out-0-trcosts.tsv"), (tetimes, 
             row = "\t".join(map(str, [times[i]] + list(dat[i])))
             fp.write(row)
             fp.write("\n")
+
+def flatten_json(d):
+    rows = []
+    for k in sorted(d.keys()):
+        rows.append([k])
+        sub = d[k]
+        for j in sorted(sub.keys()):
+            if isinstance(sub[j], list):
+                rows.append(["", j] + sub[j])
+            else:
+                rows.append(["", j, sub[j]])
+    return rows
+
+rows = flatten_json(econ)
+maxlen = max(map(len, rows))
+with open(slug + "-out-0-econ.tsv", "w+") as fp:
+    for row in rows:
+        padded = row + [""]*(maxlen-len(row))
+        fp.write("\t".join(map(str, padded)))
+        fp.write("\n")
