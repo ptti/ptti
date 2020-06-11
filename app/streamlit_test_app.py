9c73d7852c0436a78b58f20eb8f3a2095cfc1ddb
David Manheim <davidmanheim@gmail.com>
2020-06-11 21:27:46 +0300

    Redo interventions in streamlit app to be modular. TODO: Fix TTI.
    Signed-off-by: DavidManheim <davidmanheim@gmail.com>

--- app/streamlit_test_app.py
+++ app/streamlit_test_app.py
@@ -26,6 +26,80 @@ models = {}
 for ep in pkg_resources.iter_entry_points(group='models'):
     models.update({ep.name: ep.load()})
 
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=collections.OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
+
+def _eval_params(d, gvars):
+    """
+    Warning, mutates the gvars dictionary by adding parameters into it
+    """
+    params = {}
+    for k, v in d.items():
+        if isinstance(v, str):
+            params[k] = eval(v, gvars)
+        else:
+            params[k] = v
+        gvars[k] = params[k]
+        #print("setting {} to {} = {}".format(k, v, params[k]))
+    return params
+
+
+def numpy_funcs():
+    funcs = ['beta', 'binomial', 'chisquare', 'choice', 'dirichlet', 'exponential', 'gamma',
+             'geometric', 'gumbel', 'hypergeometric', 'laplace', 'logistic', 'lognormal',
+             'logseries', 'multinomial', 'multivariate_normal', 'negative_binomial',
+             'noncentral_chisquare', 'noncentral_f', 'normal', 'pareto', 'poisson', 'power',
+             'rand', 'randint', 'randn', 'random_integers', 'random_sample', 'rayleigh',
+             'standard_cauchy', 'standard_exponential', 'standard_gamma', 'standard_normal',
+             'standard_t', 'triangular', 'uniform', 'vonmises', 'wald', 'weibull', 'zipf']
+    return {f: getattr(np.random, f) for f in funcs}
+
+def interventions_load(filename=None, extant_cfg=None):
+    """
+    Load a YAML configuration file with just interventions, add it to the extant file.
+    """
+
+    if extant_cfg is not None:
+        cfg = extant_cfg
+        if filename is not None:
+            with open(filename) as fp:
+                cfg_new = ordered_load(fp.read(), yaml.FullLoader)
+                cfg['interventions'].extend(cfg_new['interventions'])
+    else:
+        raise NotImplementedError("You gotta give me an extant cfg to work with here!")
+
+    gvars = {}
+    gvars.update(numpy_funcs())
+
+    for k, v in cfg.items():
+        # collect global variables from initialisation
+        if k == "initial":
+            for i, iv in v.items():
+                gvars[i] = iv
+
+        # compute global parameters
+        if k == "parameters":
+            v.update(_eval_params(v, gvars))
+
+        if k == "interventions":
+            for intv in v:
+                for ik, iv in intv.items():
+                    if ik == "parameters":
+                        iv.update(_eval_params(iv, gvars))
+
+    return cfg
+
+
 def mkcfg(filename, sample):
     cfg = config_load(filename, sample)
 
@@ -41,7 +115,7 @@ def mkcfg(filename, sample):
 
 def pmap(f, v): return list(map(f, v))
 
-# @st.cache(suppress_st_warning=True) #Enable caching to get this to run faster, esp. for pre-run scenarios.
+@st.cache(suppress_st_warning=True) #Enable caching to get this to run faster, esp. for pre-run scenarios.
 def runSample(arg):
     i, cfg = arg
 
@@ -64,6 +138,37 @@ def runSample(arg):
 
     return tseries, events, paramtraj
 
+######################
+# Graphing misc:     #
+######################
+from matplotlib.ticker import FuncFormatter
+
+def y_fmt(y, pos):
+    decades = [1e9, 1e6, 1e3, 1e0, 1e-3, 1e-6, 1e-9 ]
+    suffix  = ["G", "M", "k", "" , "m" , "u", "n"  ]
+    if y == 0:
+        return str(0)
+    for i, d in enumerate(decades):
+        if np.abs(y) >=d:
+            val = y/float(d)
+            signf = len(str(val).split(".")[1])
+            if signf == 0:
+                return '{val:d} {suffix}'.format(val=int(val), suffix=suffix[i])
+            else:
+                if signf == 1:
+                    print(val, signf)
+                    if str(val).split(".")[1] == "0":
+                       return '{val:d} {suffix}'.format(val=int(round(val)), suffix=suffix[i])
+                tx = "{"+"val:.{signf}f".format(signf = signf) +"} {suffix}"
+                return tx.format(val=val, suffix=suffix[i])
+
+    return y
+
+def date_fmt(x, pos):  # formatter function takes tick label and tick position
+    from datetime import date
+    start = date(int(cfg['meta']['start'][0:4]), int(cfg['meta']['start'][5:7]), int(cfg['meta']['start'][8:10]))
+    return start+timedelta(days=x)
+
 st.title("PTTI Policy Simulator")
 
 st.sidebar.title("Interactive PTTI Policy Creator")
@@ -74,23 +179,27 @@ Run different epidemic control policies for COVID-19. This uses the
 """
 )
 
-POLICY_NAMES = ["1", "2a", "2b", "4a", "4b", "4c", "4d"]
-POLICY_NAMES = [name+append for name in POLICY_NAMES for append in ["","-Trig"]]
-POLICY_NAMES.append("19")
-DEFAULT_TEXT = "Model Goes Here."
+cfg = mkcfg('..\examples\structured\ptti-past.yaml', 0)
+
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem; margin-bottom: 2.5rem">{}</div>"""
 
-base_policy = st.sidebar.selectbox("Policy name", POLICY_NAMES)
+end = st.sidebar.checkbox("End Shutdown")
+TTI = st.sidebar.checkbox("Test and Trace")
+triggers = st.sidebar.checkbox("Reimpose Shutdowns As Needed")
+dance = st.sidebar.checkbox("Dance!")
+
+if end:
+    cfg = interventions_load('..\examples\structured\ptti-relax.yaml',cfg)
+
+if TTI:
+    cfg = interventions_load('..\examples\structured\ptti-tti.yaml',cfg)
+
+if triggers:
+    cfg = interventions_load('..\examples\structured\ptti-trig.yaml', cfg)
+
 to_run = st.sidebar.button("Run Model")
 # model_load_state = st.info(f"Loading policy '{base_policy}'...")
 
-filename = '..\examples\scenarios\ptti-scenario-'+ str(base_policy) + '.yaml'
-args = yaml.safe_load(open(filename))
-args['meta']['model'] = 'SEIRCTODEMem'
-args['yaml'] = filename
-cfg = mkcfg(filename,0)
-
-st.write("Scenario: " + cfg['meta']['title'])
 
 # This will be modified below.
 
@@ -109,6 +218,9 @@ st.write(str(type(Intervention_Start)) + " " + str(Intervention_Start))
 
 #import os
 # st.text(os.getcwd())
+
+if dance:
+    st.image('GIPHY_Dance.gif', caption=None, format='GIF')
 
 if to_run:
     samples = [(i, cfg) for i in range(cfg["meta"]["samples"])]
@@ -135,6 +247,12 @@ if to_run:
             df_plot_results[Compartment] = C_total.copy()
 
         plt.plot(df_plot_results)
+        ax = plt.gca()
+        ax.yaxis.set_major_formatter(FuncFormatter(y_fmt))
+        ax.xaxis.set_major_formatter(FuncFormatter(date_fmt))
+        ax.set_xlabel('Date')
+        ax.set_ylabel('People')
+
         pop = cfg['initial']['N']
         for i in cfg['interventions']:
             if ('c' in i['parameters'].keys() and 'time' in i.keys()):
